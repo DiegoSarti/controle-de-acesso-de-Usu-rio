12e347bb0c0c8eff63d9f6e64dcab9321d89f348
DiegoSarti <diegovianasa14@gmail.com>
2024-10-09 20:48:34 -0300

    Controle de Usuarios

---
+++ src/controle-de-acesso/index.js
@@ -0,0 +1,16 @@
+const usuario = 'assinante'
+
+if (usuario === 'administrador'){
+    console.log('Bem-vindo(a), administrador!')
+}else if (usuario === 'moderador'){
+    console.log('Bem-vindo(a), moderador! Você pode gerenciar conteúdo.')
+}
+else{
+switch(usuario){
+    case "assinante": return console.log('Olá, assinante! Aproveite o conteúdo disponível.')
+}
+let = usuario === '' ? console.log('perfil nao espesificado') : console.log('Acesso negado')
+}
+
+
+
